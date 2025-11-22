aa5f2e8a280d2fa7569273c6cc7962be90e8ef92
Fhysic <fhysicproductions@gmail.com>
2025-11-21 23:36:27 -0500

    Index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Analytics } from '@vercel/analytics/react';
 import QuestionFlow from './components/QuestionFlow';
 
 function App() {
-  return <QuestionFlow />;
+  return (
+    <>
+      <QuestionFlow />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
